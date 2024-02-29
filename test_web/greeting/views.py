db1b8807a4ae3da86d7421473b0f9393d49de099
Владислав Шарафутдинов <sharafutdinovsvu@gmail.com>
2024-02-29 18:21:54 +0300

    Fix

--- test_web/greeting/views.py
+++ test_web/greeting/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 
 def hello_view(request):
-    name = request.GET.get('name', 'Recruto')
-    message = request.GET.get('message', 'Давай дружить!')
+    name = request.GET.get('name', None)
+    message = request.GET.get('message', None)
+    if name is None or message is None:
+        return render(request, 'error.html')
     return render(request, 'hello.html', {'name': name, 'message': message})
